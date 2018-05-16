e6104aa01a1fcf62218104a56b75b49edde6c7c1
ntubertchen <b02902019@ntu.edu.tw>
2018-05-17 00:07:11 +0800

    feature 50x4036 per image

---
+++ feature_gen/feature_gen_tensorflowapi.py
@@ -0,0 +1,88 @@
+
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import json
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+import sys
+sys.path.append('/home/alas79923/models/research/object_detection')
+from utils import label_map_util
+MODEL_NAME = 'faster_rcnn_nas_coco_2017_11_08'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+NUM_CLASSES = 90
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+def load_train_image():
+  train_path = '/tmp2/train2014/'
+  valid_path = '/tmp2/val2014/'
+  f = open('/home/alas79923/vqa/faster-rcnn.pytorch/guesswhat.train.new.jsonl','r')
+  l = []
+  l2 = []
+  for line in f:
+    jfile = json.loads(line)
+    file_name = jfile['image']['file_name']
+    l2.append(file_name)
+    if 'train' in file_name:
+      l.append(train_path+file_name)
+    elif 'val' in file_name:
+      l.append(valid_path+file_name)
+    else:
+      print ('file name error', file_name)
+
+  return l,l2
+
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+# with detection_graph.as_default():
+#   with tf.Session(graph=detection_graph,config=config) as sess:
+#     oplist = sess.graph.get_operations()
+#     for op in oplist:
+#       if 'AvgPool' in op.name:
+#         print (op.values())
+
+# AvgPool = detection_graph.get_tensor_by_name('SecondStageBoxPredictor/AvgPool:0')
+image_paths,image_names = load_train_image()
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph,config=config) as sess:
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    AvgPool = detection_graph.get_tensor_by_name('SecondStageBoxPredictor/AvgPool:0')
+    for i in range(len(image_paths)):
+      image_path = image_paths[i]
+      print (image_path)
+      image = Image.open(image_path)
+      image_np = load_image_into_numpy_array(image)
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      a = sess.run([AvgPool],feed_dict={image_tensor: image_np_expanded})
+      feature = np.squeeze(np.array(a[0]))
+      np.savetxt('/tmp2/train_nas_feature/'+image_names[i]+'.out',feature)
+    # /tmp2/train2014/COCO_train2014_000000291822.jpg
+    # /home/alas79923/vqa/faster-rcnn.pytorch/
